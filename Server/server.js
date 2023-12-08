982bfffbd3c3f8327d1581ebba53a1115e209a52
praju-1 <prajaktatayde26@gmail.com>
2023-12-08 14:16:16 +0530

    Updated

--- Server/server.js
+++ Server/server.js
@@ -5,7 +5,14 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors(
+  {
+    origin:[],
+    methods:['POST', 'GET'],
+    credentials:true
+
+  }
+));
 app.use(express.json());
 
 mongoose.connect('mongodb+srv://prajakta:praju%40123@cluster0.jz314qu.mongodb.net/todo?retryWrites=true&w=majority');
