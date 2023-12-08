cee11d86caef27729725ff6ab05a23ebbbbd3db1
praju-1 <prajaktatayde26@gmail.com>
2023-12-08 22:30:38 +0530

    updated

--- Server/server.js
+++ Server/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const env = require("dotenv");
+env.config()
 
 const app = express();
 const port = process.env.PORT || 5000;
+
 
 app.use(cors(
   {
@@ -15,7 +18,7 @@ app.use(cors(
 ));
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://prajakta:praju%40123@cluster0.jz314qu.mongodb.net/todo?retryWrites=true&w=majority');
+mongoose.connect(process.env.Mongo_url);
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log('MongoDB connection established successfully');
